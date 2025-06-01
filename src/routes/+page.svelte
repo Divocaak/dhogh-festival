73ea960799c60c71d14873f0a6c1150aa87adf38
Vojtěch divoký <divokyvojta@gmail.com>
2025-06-01 21:20:34 +0200

    seo #5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,198 @@
 	const ticketColors = ['var(--red)', 'var(--violet)', 'var(--green)'];
 </script>
 
+<svelte:head>
+	<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "Festival",
+			"name": "DHOGH Festival 2025",
+			"startDate": "2025-06-18",
+			"endDate": "2025-06-20",
+			"location": {
+				"@type": "Place",
+				"name": "Žižkárna",
+				"url": "https://zizkarna.cz",
+				"address": {
+					"@type": "PostalAddress",
+					"streetAddress": "Žižkova tř. 28",
+					"addressLocality": "České Budějovice",
+					"addressCountry": "CZ"
+				}
+			},
+			"description": "Festival propojuje taneční hudbu, umění, filozofii a komunitního ducha undergroundové scény. Poskytuje nejen kulturní zážitek, ale i prostor pro hlubší reflexi nad významem elektronické hudby a jejího vlivu na městský noční život.",
+			"eventSchedule": [
+				{
+					"@type": "Event",
+					"name": "Středa - Local DJs, Ableton Workshop, DJ Workshop",
+					"startDate": "2025-06-18",
+					"location": {
+						"@type": "Place",
+						"name": "Žižkárna",
+						"url": "https://zizkarna.cz",
+						"address": {
+							"@type": "PostalAddress",
+							"streetAddress": "Žižkova tř. 28",
+							"addressLocality": "České Budějovice",
+							"addressCountry": "CZ"
+						}
+					},
+					"performer": [
+						{
+							"@type": "MusicGroup",
+							"name": "body_bass"
+						},
+						{
+							"@type": "Person",
+							"name": "antonin_padrta"
+						},
+						{
+							"@type": "Event",
+							"name": "DJ Workshop"
+						}
+					]
+				},
+				{
+					"@type": "Event",
+					"name": "Čtvrtek - Live Sets, Synth Workshop",
+					"startDate": "2025-06-19",
+					"location": {
+						"@type": "Place",
+						"name": "Žižkárna",
+						"url": "https://zizkarna.cz",
+						"address": {
+							"@type": "PostalAddress",
+							"streetAddress": "Žižkova tř. 28",
+							"addressLocality": "České Budějovice",
+							"addressCountry": "CZ"
+						}
+					},
+					"performer": [
+						{
+							"@type": "MusicGroup",
+							"name": "Jadran"
+						},
+						{
+							"@type": "Person",
+							"name": "mista_humix"
+						},
+						{
+							"@type": "MusicGroup",
+							"name": "BIOTOP"
+						},
+						{
+							"@type": "Person",
+							"name": "Tom_Holič"
+						},
+						{
+							"@type": "Organization",
+							"name": "Synth Library Prague"
+						}
+					]
+				},
+				{
+					"@type": "Event",
+					"name": "Pátek - Open Air (17:00-22:00)",
+					"startDate": "2025-06-20T17:00",
+					"endDate": "2025-06-20T22:00",
+					"location": {
+						"@type": "Place",
+						"name": "Žižkárna",
+						"url": "https://zizkarna.cz",
+						"address": {
+							"@type": "PostalAddress",
+							"streetAddress": "Žižkova tř. 28",
+							"addressLocality": "České Budějovice",
+							"addressCountry": "CZ"
+						}
+					},
+					"performer": [
+						{
+							"@type": "Person",
+							"name": "TIGERHEAD",
+							"sameAs": "https://ra.co/dj/tigerhead"
+						},
+						{
+							"@type": "Person",
+							"name": "ALEX_WILCOX",
+							"sameAs": "https://www.discogs.com/artist/8125675-Alex-Wilcox"
+						},
+						{
+							"@type": "Person",
+							"name": "WNCHNZ"
+						},
+						{
+							"@type": "Person",
+							"name": "Surrealita",
+							"sameAs": "https://ra.co/dj/surrealita"
+						},
+						{
+							"@type": "Person",
+							"name": "Dj_Alyaz",
+							"sameAs": "https://ra.co/dj/djalyaz"
+						},
+						{
+							"@type": "Person",
+							"name": "Mor4m"
+						},
+						{
+							"@type": "Person",
+							"name": "VEIT B",
+							"sameAs": "https://soundcloud.com/veit-b"
+						},
+						{
+							"@type": "Person",
+							"name": "tvyks"
+						}
+					]
+				}
+			],
+			"offers": [
+				{
+					"@type": "Offer",
+					"name": "Full Festival Pass",
+					"description": "Vstupenka na všechny festivalové dny včetně páteční afterparty",
+					"price": "500",
+					"priceCurrency": "CZK",
+					"availability": "https://schema.org/InStock",
+					"url": "https://goout.net/cs/dhogh-festival-2025/szjdaux/"
+				},
+				{
+					"@type": "Offer",
+					"name": "Friday Open Air",
+					"description": "Vstupenka na páteční program na hlavní stage, kde vystoupí oba headlineři",
+					"price": "300",
+					"priceCurrency": "CZK",
+					"availability": "https://schema.org/InStock",
+					"url": "https://goout.net/cs/dhogh-festival-2025/szjdaux/"
+				},
+				{
+					"@type": "Offer",
+					"name": "Wormuper (Středa/Čtvrtek)",
+					"description": "Vstupenka na středeční a čtvrteční program zaměřený na vzdělávání a komunitu",
+					"price": "100-200",
+					"priceCurrency": "CZK",
+					"availability": "https://schema.org/InStock",
+					"url": "https://goout.net/cs/dhogh-festival-2025/szjdaux/"
+				}
+			],
+			"organizer": {
+				"@type": "Organization",
+				"name": "DHOGH Team",
+				"email": "dhoghteam@gmail.com",
+				"url": "https://dhogh.com"
+			},
+			"image": [
+				"https://dhogh.com/imgs/artists/alex.jpg",
+				"https://dhogh.com/imgs/artists/tigerhead.jpg",
+				"https://dhogh.com/imgs/artists/wnchnz.jpg",
+				"https://dhogh.com/imgs/values.jpg"
+			],
+			"url": "https://dhogh.com"
+		}
+	</script>
+</svelte:head>
+
 <div class="lang-btn-wrapper">
 	<Button
 		label={$lang === 'cs' ? 'en' : 'cz'}
